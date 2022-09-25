3a9482f359a8a2971e85f27931acea1f0eb2c245
Tamal Web <tamalanwaronline@gmail.com>
2022-09-25 23:03:54 +0600

    Tests the app for the article

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './scss/index.scss'
+import Card from './Card'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+function Card() {
+  return (
+    <div className="card">
+      <div className="card__title">Wonderful Card Title Here</div>
+      <div className="card__body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+        suscipit dicta ea reiciendis, vero veniam atque quia quas provident
+        impedit.
+      </div>
+      <div className="card__button--active">
+        <button>Lets Do It</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
